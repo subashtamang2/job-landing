b41060983065854d87ab76d6d56e997d867ac440
SubashLama123 <lamasubaah2@gmail.com>
2025-07-07 15:36:56 +0545

    complete navbar

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -20,17 +20,18 @@ const Nav = ({ openNav }: Props) => {
     window.addEventListener("scroll", handler);
     return () => window.removeEventListener("scroll", handler);
   }, []);
+
   return (
-    <div className={`transition-all  ${navBg ? "bg-white shadow-md" : "fixed"} duration-200 h-[12vh] z-[10000] fixed w-full `} >
+    <div className={`transition-all  ${navBg ? "bg-white dark:bg-gray-900 shadow-md" : "fixed"} duration-200 h-[12vh] z-[10000] fixed w-full `} >
       <div className='flex items-center h-full justify-between w-[92%] mx-auto'>
         <div className='flex items-center sm:space-x-20'>
 
           {/* Logo */}
           <div className='flex items-center space-x-2'>
-            <div className='w-10 h-10 bg-cyan-800 rounded-full flex items-center justify-center flex-col'>
-              <LuNetwork className='w-5 h-5 text-white' />
+            <div className='w-10 h-10 bg-cyan-800 dark:bg-white rounded-full flex items-center justify-center flex-col'>
+              <LuNetwork className='w-5 h-5 text-white dark:text-black' />
             </div>
-            <h1 className='text-xl hidden sm:block md:text-2xl text-cyan-800 font-bold'>DevHire</h1>
+            <h1 className='text-xl hidden sm:block md:text-2xl text-cyan-800 dark:text-white font-bold'>DevHire</h1>
           </div>
 
           {/* NVLINKS */}
@@ -48,7 +49,7 @@ const Nav = ({ openNav }: Props) => {
         {/* Buttons */}
         <div className='flex items-center space-x-4'>
           {/* Login/register button */}
-          <button className='px-8 py-2.5 text-xs sm:text-sm rounded-lg cursor-pointer bg-gray-100 hover:bg-gray-300 transition-all duration-300'>Login / Register</button>
+          <button className='px-8 py-2.5 text-xs sm:text-sm rounded-lg cursor-pointer bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-900 hover:bg-gray-300 transition-all duration-300'>Login / Register</button>
 
           {/* Job post Button */}
           <button className='px-8 py-2.5 text-sm hidden sm:block cursor-pointer rounded-lg bg-cyan-700 hover:bg-cyan-900 transition-all duration-300 text-white'>Job Post</button>
