ba87e043dae769dad8a2c641faa7510d380d0d29
SubashLama123 <lamasubaah2@gmail.com>
2025-07-07 16:58:36 +0545

    search

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import JobSearchBar from './JobSearchBar'
 
 const Hero = () => {
   return (
@@ -6,8 +7,12 @@ const Hero = () => {
       <div className='w-[90%] md:w-[80%] mx-auto items-center grid grid-cols-1 xl:grid-cols-2 gap-10'>
         {/* Text Content */}
         <div>
+          {/* heading */}
           <h1 className='text-3xl sm:text-6xl font-bold'>join us & Explore Thousnads of jobs</h1>
+          {/* subheading */}
           <p className='mt-4 text-sm sm:text-lg font-medium'>Find jobs, Employment  & Career Opportunities</p>
+          {/* jobsearchBar */}
+          <JobSearchBar />
 
         </div>
         {/* image content */}
--- components/Home/Hero/JobSearchBar.tsx
+++ components/Home/Hero/JobSearchBar.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { FaMap } from 'react-icons/fa'
+import { MdSearch } from 'react-icons/md'
 
 const JobSearchBar = () => {
   return (
-    <div>JobSearchBar</div>
+    <div className='w-full max-w-4xl mx-auto mt-8'>
+      <div className='flex flex-col md:flex-row bg-white dark:bg-gray-900 shadow-md rounded-lg overflow-hidden'>
+        {/* what fields */}
+        <div className='flex items-center border-b md:border-b-0 md:border-r border-b-gray-200 dark:border-gray-700 px-4 sm:py-6 py-3 w-full md:w-1/2'>
+          <MdSearch className='text-gray-500 text-xl mr-2' />
+          <input
+            type='text'
+            placeholder='Job title or company' className='w-full outline-none' />
+        </div>
+        {/* where fields */}
+        <div className='flex items-center border-b md:border-b-0 border-gray-200 dark:border-gray-700 px-4 sm:py-6 py-3 w-full md:w-1/2'>
+          <FaMap className='text-gray-500 text-xl mr-2' />
+          <input type='text' placeholder='City or postcode'
+            className='w-full outline-none' />
+        </div>
+        {/* find jobs button */}
+        <button className='bg-blue-600 text-white px-8 sm:py-6 py-3 cursor-pointer text-sm md:text-base w-full md:w-auto min-w-[140px] whitespace-nowrap hover:bg-blue-700 transition'>Find Jobs</button>
+      </div>
+
+    </div>
   )
 }
 
