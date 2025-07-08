8a2267da8e99621db7562b9937ee0cb533c43450
SubashLama123 <lamasubaah2@gmail.com>
2025-07-08 20:04:35 +0545

    product data

--- components/Home/Job/Job.tsx
+++ components/Home/Job/Job.tsx
@@ -1,4 +1,82 @@
 import React from 'react'
+const jobs = [
+  {
+    id: 1,
+    image: "/images/j1.png",
+    title: "Software Engineer",
+    location: "London, UK",
+    jobType: "Full Time",
+    urgency: "Urgent",
+  },
+
+  {
+    id: 2,
+    image: "/images/j2.png",
+    title: "Recruiting Coordinator",
+    location: "Manchester, UK",
+    jobType: "Part Time",
+    urgency: "Urgent",
+  },
+  {
+    id: 3,
+    image: "/images/j3.png",
+    title: "Product Manager, Studio",
+    location: "Birmingham, UK",
+    jobType: "Contract",
+    urgency: "Urgent",
+  },
+  {
+    id: 4,
+    image: "/images/j4.png",
+    title: "Senior Product Designer",
+    location: "Bristol, UK",
+    jobType: "Full Time",
+    urgency: "Urgent",
+  },
+  {
+    id: 5,
+    image: "/images/j5.png",
+    title: "Product Manager,Risk",
+    location: "Edinburgh, UK",
+    jobType: "Contract",
+    urgency: "private",
+  },
+  {
+    id: 6,
+    image: "/images/j6.png",
+    title: "Technical Architect",
+    location: "Leeds, UK",
+    jobType: "Full Time",
+    urgency: "Private",
+  },
+  {
+    id: 7,
+    image: "/images/j7.png",
+    title: "Web Developer",
+    location: "Bringhton, UK",
+    jobType: "Part Time",
+    urgency: "Urgent",
+  },
+  {
+    id: 8,
+    image: "/images/j8.png",
+    title: "Senior product Designer",
+    location: "Glasgow, UK",
+    jobType: "Contract",
+    urgency: "Private",
+  },
+  {
+    id: 9,
+    image: "/images/j9.png",
+    title: "Senior BI Analyst",
+    location: "Sheffield, UK",
+    jobType: "Full Time",
+    urgency: "Private",
+  },
+
+
+]
+
 
 const Job = () => {
   return (
