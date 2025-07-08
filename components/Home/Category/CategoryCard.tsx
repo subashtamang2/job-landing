f578aab9b88a05e4e940b47ab6f03f9c9dcc5c52
SubashLama123 <lamasubaah2@gmail.com>
2025-07-08 19:53:48 +0545

    complete category page

--- components/Home/Category/CategoryCard.tsx
+++ components/Home/Category/CategoryCard.tsx
@@ -18,7 +18,8 @@ const CategoryCard = ({ category }: Props) => {
       <div className='w-16 h-16 bg-gray-200 dark:bg-gray-800 rounded-full mx-auto flex items-center justify-center flex-col'>
         {category.icon}
       </div>
-
+      <h1 className='mt-6 text-center font-medium text-gray-800 dark:text-gray-200'>{category.categoryName}</h1>
+      <p className='mt-2 text-gray-600 dark:text-gray-400 text-sm text-center'>({category.openPositions} Open Position)</p>
     </div>
   </Tilt>
 }
