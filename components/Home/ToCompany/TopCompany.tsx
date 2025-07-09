91277a4b08fb9b883c879d59e714c1e1bbaef08e
SubashLama123 <lamasubaah2@gmail.com>
2025-07-09 10:40:02 +0545

    top company

---
+++ components/Home/ToCompany/TopCompany.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+const CompanyData = [
+  {
+    id: 1,
+    image: "/images/c1.png",
+    name: "Udemy",
+    location: "London, UK",
+    position: "20",
+  },
+  {
+    id: 2,
+    image: "/images/c1.png",
+    name: "Stripe",
+    location: "Kolkata, India",
+    position: "30",
+  },
+  {
+    id: 3,
+    image: "/images/c3.png",
+    name: "Dropbox",
+    location: "Lahore, Pakistan",
+    position: "33",
+  },
+  {
+    id: 4,
+    image: "/images/c4.png",
+    name: "Figma",
+    location: "Dhaka, Bangaladesh",
+    position: "40",
+  },
+];
+
+
+
+const TopCompany = () => {
+  return (
+    <div>TopCompany</div>
+  )
+}
+
+export default TopCompany
